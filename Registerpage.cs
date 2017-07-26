8bcd36688064c3ad3c311006b870b4c4e303c1b6
Si-143 <30472917+Si-143@users.noreply.github.com>
2017-07-26 19:51:46 +0100

    Update Registerpage.cs

--- Registerpage.cs
+++ Registerpage.cs
@@ -1,18 +1,24 @@
+        protected void Register_Click(object sender, EventArgs e)
+        {
+           
+            string N = Name.Text;
+            string U = User.Text;
+            string P = Pass.Text;
+            string E = Email.Text;
+           // assign the textboxes into variables 
+            if (Name.Text =="" || Pass.Text =="" || Pass.Text == "" || Email.Text == ""){// check if either the textboxs are blank. 
+            Output.Text = ("try again");
+            }
+            else
+            {
+                DB.Register(N,E,U,P);// calls the register methods 
+                Response.Redirect("Login");// redirect to the Login page with sucesssful 
+            }
 
-  protected void Register_Click(object sender, EventArgs e)
- {
-string N = Name.Text; string U = User.Text; string P = Pass.Text;
-string E = Email.Text;
-// assign the textboxes into variables
-if (Name.Text =="" || Pass.Text =="" || Pass.Text == "" ||
-Email.Text == ""){// check if either the textboxs are blank.
-Output.Text = ("try again");
+  
 }
-else
-{
-DB.Register(N,E,U,P);// calls the register methods
-Response.Redirect("Login");// redirect to the Login page with
-sucesssful
-}
- }
-} }
+   
+        }
+
+        
+    }
